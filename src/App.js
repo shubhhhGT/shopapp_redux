1ed23d51dbb9de7552cf9d04bb428003a3401db2
shubhhhGT <shubhamgt001@gmail.com>
2023-10-05 01:47:44 +0530

    Adding App Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {Routes, Route} from 'react-router-dom'
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <div className='bg-slate-900'>
+        <Navbar/>
+      </div>
+
+      {/* Define the routing for different pages using React Router. */}
+      <Routes>
+        {/* Route to the Home page. */}
+        <Route path='/' element={<Home/>} />
+        {/* Route to the cart page. */}
+        <Route path='/cart' element={<Cart/>} />
+      </Routes>
+
     </div>
   );
 }
